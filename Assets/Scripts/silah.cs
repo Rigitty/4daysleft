26e29fe5a647967ac5ba3f9c4c32ddaea58d9122
rigitty <yararsamet04@gmail.com>
2023-11-07 18:44:17 +0300

    Gun animated

--- Assets/Scripts/silah.cs
+++ Assets/Scripts/silah.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class silah : MonoBehaviour
@@ -8,33 +9,27 @@ public class silah : MonoBehaviour
     public GameObject mermi1;
     public Transform barrel;
     private float time1;
-    private bool isshooted=false;
-    private float delay;
-    public float delaytime;
-    public ParticleSystem shoteffect;
+    public GameObject shoteffect;
+    public GameObject arm;
+    public float cooltimedefault;
+    private float cooltime;
     void Update()
     {
         //timer
         time1 = time1 + Time.deltaTime;
-        delay = delay + Time.deltaTime;
+        if (cooltime > 0)
+        {
+           cooltime = cooltime - Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown("space")){
-            if (!isshooted)
-            {
+        if (Input.GetKeyDown("space") && cooltime<=0){
+                cooltime = cooltimedefault;
                 //clone mermi
                 Instantiate(mermi1, barrel.position, barrel.rotation);
                 //clone particle
                 Instantiate(shoteffect, barrel.position, barrel.rotation);
-                isshooted = true;
-            }
-            else
-            {
-                if (delay > delaytime)
-                {
-                    isshooted = false;
-                    delay = 0;
-                }
-            }
+                //animate
+                arm.GetComponent<Animator>().SetTrigger("shot");
         }
 
     }
